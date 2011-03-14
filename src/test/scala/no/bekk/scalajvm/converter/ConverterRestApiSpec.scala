3379e3c8f65484bb8c8204338b47e33cc5124f43
Frode Nerbråten <frode@nerbraten.no>
2011-03-14 16:35:34 +0100

    Added more specs

--- src/test/scala/no/bekk/scalajvm/converter/ConverterRestApiSpec.scala
+++ src/test/scala/no/bekk/scalajvm/converter/ConverterRestApiSpec.scala
@@ -1,7 +1,7 @@
 package no.bekk.scalajvm.converter
 
 import org.scalatra.test.specs.ScalatraSpecification
-import xml.XML
+import xml.{NodeSeq, XML}
 
 class ConverterRestApiSpec extends ScalatraSpecification {
   addFilter(classOf[ConverterRestApi], "/*")
@@ -11,18 +11,35 @@ class ConverterRestApiSpec extends ScalatraSpecification {
   "The Conversion rest api" should {
 
     "when using GET / return a welcome message with a link to the celsius-to-fahrenheit conversing" in {
-      get("/") {
+      get(rootRoute) {
         status mustEqual(200)
         val xmlBody = XML.loadString(body)
         (xmlBody \\ "a" \ "@href").text mustEqual(celsiusToFahrenheitRoute)
       }
     }
 
-    "when using GET /celsius/fahrenheit return a sample conversion chart" in {
-
+    "when using GET " + celsiusToFahrenheitRoute + " return a sample conversion chart" in {
+      get(celsiusToFahrenheitRoute) {
+        val xmlBody = XML.loadString(body)
+        val tableHeaders = xmlBody \\ "th"
+        tableHeaders(0).text mustEqual("Celsius")
+        tableHeaders(1).text mustEqual("Fahrenheit")
+        val tableValues = xmlBody \\ "td"
+        tableValues.foreach(_.text mustMatch """[0-9]+(\.[0-9]+)?""")
+      }
     }
 
+    "when using GET "+ celsiusToFahrenheitRoute +"/:number return the converted result" in {
+      get(celsiusToFahrenheitRoute + "/50") {
+        body mustEqual "122.0"
+      }
+    }
 
+    "when using GET "+ fahrenheitToCelsiusRoute +"/:number return the converted result" in {
+      get(fahrenheitToCelsiusRoute + "/-4") {
+        body mustEqual "-20.0"
+      }
+    }
 
   }
 }
